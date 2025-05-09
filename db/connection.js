62a57703a21c0c81b30def51d8449d2ce746bf2b
Davi942 <davi.henrique@sempreceub.com>
2025-05-08 22:51:00 -0300

    Commit inicial

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,19 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize({
-    dialect : "sqlite" ,
-    storage : "./db/app.db"
-});
+const sequelize = new Sequelize(
+    "postgres", // Nome do banco de dados (geralmente "postgres" no Supabase)
+    "postgres", // Usuário do banco (geralmente "postgres" no Supabase)
+    "[Doris123!]", // Senha do banco de dados
+    {
+        host: "db.gudngldxnlftrfqndoor.supabase.co", // Host fornecido pelo Supabase
+        dialect: "postgres", // Dialeto para PostgreSQL
+        port: 5432, // Porta padrão do PostgreSQL
+        logging: false, // Desativa logs de SQL (opcional)
+        ssl: {
+            require: true, // Exige SSL para conexão segura
+            rejectUnauthorized: false, // Permite certificados autoassinados
+        },
+    }
+);
 
-module.exports = sequelize
+module.exports = sequelize;
